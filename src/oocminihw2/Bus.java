e0fd4c49187e68c33f40a81c91283a2f7ff13a85
StefNunes <2022255@student.cct.ie>
2023-10-14 15:41:33 +0100

    one class for each interface
    bus, sailboat and airplane

---
+++ src/oocminihw2/Bus.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocminihw2;
+
+import oocminihw2.Drivable;
+import oocminihw2.Vehicle;
+
+/**
+ *
+ * @author Stefani Nunes
+ */
+public class Bus extends Vehicle implements Drivable {
+    public Bus(String make, String type, int numPassengers) {
+        this.make = make;
+        this.type = type;
+        this.numWheels = 4;
+        this.numPassengers = numPassengers;
+    }
+
+    @Override
+    public void accelerate(float speed) {
+        // Implement the acceleration logic for a car.
+    }
+
+    @Override
+    public void brake() {
+        // Implement the braking logic for a car.
+    }
+
+    @Override
+    public void turn(float angle) {
+        // Implement the turning logic for a car.
+    }
+
+    @Override
+    public float getDirection() {
+        return direction;
+    }
+
+    @Override
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public String getMake() {
+        return make;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+}
